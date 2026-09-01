6a9b89881c28a2d3bff13a4c346f00ab70bf86f7
HenrieMason <masonhenrie2003@gmail.com>
2026-08-31 19:45:29 -0700

    Improve landing page design

--- app/api/shopify-webhook/route.ts
+++ app/api/shopify-webhook/route.ts
@@ -530,8 +530,8 @@ async function createPrintifyOrder({
             {
             id: imageData.id,
             x: 0.5,
-            y: 0.49,//43
-            scale: 0.92,
+            y: 0.46,//43
+            scale: 0.92, //86
             angle: 0,
             },
         ],
