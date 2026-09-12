0b50275a27e27894ff494bdecc20ca03b78ce261
HenrieMason <masonhenrie2003@gmail.com>
2026-09-11 21:44:37 -0700

    Improve landing page design

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/dream6",
+        destination: "https://dream3.carsceneapparel.com",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
